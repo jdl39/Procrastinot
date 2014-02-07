cb3e2e707ea811b7bd351aa725279dd7b0b320b1
Jimmy Lee <jdl39@stanford.edu>
2014-02-07 00:15:24 +0000

    Built a basic homepage.

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,30 @@
 exports.view = function(req, res){
-	res.render('index');
+	res.render('index', {
+		'buttons' : [
+			{
+				'name' : 'Profile',
+				'imgFile' : 'profileStub.png',
+				'url' : '/',
+				'description' : 'Here you will find information about your account.',
+			},
+			{
+				'name' : 'Projects',
+				'imgFile' : 'projectsStub.png',
+				'url' : '/',
+				'description' : 'Here you can manage your current projects',
+			},
+			{
+				'name' : 'Leaderboards',
+				'imgFile' : 'leaderboardsStub.png',
+				'url' : '/',
+				'description' : 'Here you can see how you rank against your friends and the world!',
+			},
+			{
+				'name' : 'Help',
+				'imgFile' : 'helpStub.png',
+				'url' : '/',
+				'description' : 'Press this at any time to return to this screen.',
+			},
+		]
+	});
 };
