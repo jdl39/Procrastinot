ae42eeae0755265ddf8b0952d00131d9dac269b6
Jimmy Lee <jdl39@stanford.edu>
2014-02-07 07:22:47 +0000

    Buttonized navbar and added to profile page

--- routes/profile.js
+++ routes/profile.js
@@ -2,5 +2,11 @@ var data = require('../public/js/contexts.js').data
 
 exports.view = function(req, res){
 	
+	// STUBBED DATA
+	data['username'] = "Bob"
+	data['imgURL'] = 'http://static4.businessinsider.com/image/50364bc869bedd7e37000015/meet-foursquares-money-man-steven-rosenblatt.jpg'
+	data['level'] = 3
+	data['points'] = 9001
+
 	res.render('profile', data);
 };
