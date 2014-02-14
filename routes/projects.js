33639f7a5e4dbb9e9a1e1f96a86578630b338bf6
Jimmy Lee <jdl39@stanford.edu>
2014-02-14 02:09:43 +0000

    More profile / Made projects completeable

--- routes/projects.js
+++ routes/projects.js
@@ -1,6 +1,69 @@
 var data = require('../public/js/contexts.js').data
 
 exports.viewProjects = function(req, res){
+
+	data['currentProjects'] = [
+		{
+			"name" : "CS147 Assignment 6",
+			"points" : 200,
+			"dueDate" : "02/13/2014"
+		},
+		{
+			"name" : "CS147 Assignment 7",
+			"points" : 200,
+			"dueDate" : "02/20/2014"
+		},
+		{
+			"name" : "CS147 Assignment 8",
+			"points" : 200,
+			"dueDate" : "02/27/2014"
+		},
+		{
+			"name" : "CS147 Assignment 6",
+			"points" : 200,
+			"dueDate" : "02/13/2014"
+		},
+		{
+			"name" : "CS147 Assignment 7",
+			"points" : 200,
+			"dueDate" : "02/20/2014"
+		},
+		{
+			"name" : "CS147 Assignment 8",
+			"points" : 200,
+			"dueDate" : "02/27/2014"
+		},
+		{
+			"name" : "CS147 Assignment 6",
+			"points" : 200,
+			"dueDate" : "02/13/2014"
+		},
+		{
+			"name" : "CS147 Assignment 7",
+			"points" : 200,
+			"dueDate" : "02/20/2014"
+		},
+		{
+			"name" : "CS147 Assignment 8",
+			"points" : 200,
+			"dueDate" : "02/27/2014"
+		},
+		{
+			"name" : "CS147 Assignment 6",
+			"points" : 200,
+			"dueDate" : "02/13/2014"
+		},
+		{
+			"name" : "CS147 Assignment 7",
+			"points" : 200,
+			"dueDate" : "02/20/2014"
+		},
+		{
+			"name" : "CS147 Assignment 8",
+			"points" : 200,
+			"dueDate" : "02/27/2014"
+		}
+	]
 	
 	res.render('projects', data);
 };
