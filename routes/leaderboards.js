d515da703c5b4fca65946759e09b5aafa591578c
vagrant <vagrant@precise32.(none)>
2014-02-14 01:57:47 +0000

    fleshing out leaderboard

--- routes/leaderboards.js
+++ routes/leaderboards.js
@@ -2,5 +2,94 @@ var data = require('../public/js/contexts.js').data
 
 exports.view = function(req, res){
 	
+
+	data['friends'] = [
+	{
+		"name" : "Friend 1",
+		"points" : "500",
+		"level" : "6",
+		"imageURL" : "http://lorempixel.com/output/people-q-c-640-480-5.jpg",
+		"profileURL" : "/profile"
+	},
+	{
+		"name" : "Friend 2",
+		"points" : "400",
+		"level" : "5",
+		"imageURL" : "http://lorempixel.com/output/people-q-c-640-480-5.jpg",
+		"profileURL" : "/profile"
+	},
+	{
+		"name" : "Friend 3",
+		"points" : "300",
+		"level" : "4",
+		"imageURL" : "http://lorempixel.com/output/people-q-c-640-480-5.jpg",
+		"profileURL" : "/profile"
+	},
+	{
+		"name" : "Friend 4",
+		"points" : "200",
+		"level" : "3",
+		"imageURL" : "http://lorempixel.com/output/people-q-c-640-480-5.jpg",
+		"profileURL" : "/profile"
+	},
+	{
+		"name" : "Friend 5",
+		"points" : "500",
+		"level" : "6",
+		"imageURL" : "http://lorempixel.com/output/people-q-c-640-480-5.jpg",
+		"profileURL" : "/profile"
+	},
+	{
+		"name" : "Friend 6",
+		"points" : "400",
+		"level" : "5",
+		"imageURL" : "http://lorempixel.com/output/people-q-c-640-480-5.jpg",
+		"profileURL" : "/profile"
+	},
+	{
+		"name" : "Friend 7",
+		"points" : "300",
+		"level" : "4",
+		"imageURL" : "http://lorempixel.com/output/people-q-c-640-480-5.jpg",
+		"profileURL" : "/profile"
+	},
+	{
+		"name" : "Friend 8",
+		"points" : "200",
+		"level" : "3",
+		"imageURL" : "http://lorempixel.com/output/people-q-c-640-480-5.jpg",
+		"profileURL" : "/profile"
+	},
+	{
+		"name" : "Friend 9",
+		"points" : "500",
+		"level" : "6",
+		"imageURL" : "http://lorempixel.com/output/people-q-c-640-480-5.jpg",
+		"profileURL" : "/profile"
+	},
+	{
+		"name" : "Friend 10",
+		"points" : "400",
+		"level" : "5",
+		"imageURL" : "http://lorempixel.com/output/people-q-c-640-480-5.jpg",
+		"profileURL" : "/profile"
+	},
+	{
+		"name" : "Friend 11",
+		"points" : "300",
+		"level" : "4",
+		"imageURL" : "http://lorempixel.com/output/people-q-c-640-480-5.jpg",
+		"profileURL" : "/profile"
+	},
+	{
+		"name" : "Friend 12",
+		"points" : "200",
+		"level" : "3",
+		"imageURL" : "http://lorempixel.com/output/people-q-c-640-480-5.jpg",
+		"profileURL" : "/profile"
+	},
+
+	]
+
 	res.render('leaderboards', data);
 };
